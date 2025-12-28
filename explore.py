c5a1bd18b4db04e1bbc1510bc68d55e2deb3453a
Golbarg-max <golbarg1384@gmail.com>
2025-12-28 18:14:32 -0500

    Clean data and insert into database

--- explore.py
+++ explore.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import mysql.connector
+
 df = pd.read_csv('trx-10k.csv')
 
 
@@ -29,3 +31,21 @@ print(df_clean['status'].unique())
 print(df_clean['card_type'].unique())
 print(df_clean['city'].unique())
 print(df_clean.info())
+
+df_clean = df_clean.drop('id', axis=1)
+df_clean['transaction_id'] = range(1, len(df_clean) + 1)
+df_clean = df_clean.where(pd.notna(df_clean), None)
+
+cnx  = mysql.connector.connect(
+    user='root', 
+    password = 'pass245word', 
+    host = 'localhost',
+    database = 'transactions_db')
+cursor = cnx.cursor()
+cursor.execute("TRUNCATE TABLE transactions;")
+sql_query = """INSERT INTO transactions (transaction_id, status, time, card_type, city, amount) 
+            VALUES (%s, %s, %s, %s, %s, %s);"""
+for index, row in df_clean.iterrows():
+    cursor.execute(sql_query, (row['transaction_id'], row['status'], row['time'], row['card_type'], row['city'], row['amount']))
+cnx.commit()
+cursor.close()
